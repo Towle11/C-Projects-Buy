2307f26d080be0284415381f5027c25bf94a9125
Abdi Shakur Omar <75699810+Towle11@users.noreply.github.com>
2022-05-10 06:58:20 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // let menu = document.querySelector('#menu-bar');
 // let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
-}
+// menu.onclick = () =>{
+//   menu.classList.toggle('fa-times');
+//   navbar.classList.toggle('active');
+// }
 
-window.onscroll = () =>{
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
-}
+// window.onscroll = () =>{
+//   menu.classList.remove('fa-times');
+//   navbar.classList.remove('active');
+// }
 
 // let slides = document.querySelectorAll('.slide-container');
 // let index = 0;
